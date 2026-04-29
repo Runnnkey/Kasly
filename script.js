197bf5807ff7118e3108de6e64ad7da9173882de
Oktora Rizka Arifin <oktorarizka19@gmail.com>
2026-04-29 13:20:06 +0800

    update menu jadi dinamis

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const menuBtn = document.getElementById("menuBtn");
+const menuBtn2 = document.getElementById("menuBtn2");
 const sidebar = document.getElementById("sidebar");
 
 menuBtn.addEventListener("click", () => {
+    sidebar.classList.toggle("-translate-x-full");
+});
+
+menuBtn2.addEventListener("click", () => {
     sidebar.classList.toggle("-translate-x-full");
 });
